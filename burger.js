be59b40b63ac9bc617be8e3b3f1dec96452fe634
Anton-Ostrovskiy <ostrovskiy.98@mail.ru>
2023-02-25 00:10:06 +0300

    add header bc

--- burger.js
+++ burger.js
@@ -5,3 +5,19 @@ for (let tag of list)
 tag.addEventListener("click", function (event) {
     burger.checked = false;
 })
+
+
+function scrollValue() {
+    let header = document.getElementById("header");
+    let intro = document.getElementById("intro");
+    let introH = intro.clientHeight;
+    let scroll = window.scrollY;
+
+    if (scroll > introH) {
+        header.classList.add("fixed");
+    } else {
+        header.classList.remove("fixed");
+    }
+}
+
+window.addEventListener("scroll", scrollValue);
